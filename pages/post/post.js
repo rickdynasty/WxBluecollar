8891284f1e4711eaf1489193331ec50212b0a5bf
rick_tan <rick_tan@qq.com>
2019-10-30 08:08:02 +0800

    update for 数据绑定

--- pages/post/post.js
+++ pages/post/post.js
@@ -5,21 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    /*object:{
+      date:"29 2019"
+    },
+    title:"冰棍和雪糕",
+    postImg: "/images/post/post-4.jpg",
+    avatar:"/images/avatar/avatar-5.png",
+    content:"冰棒： 有木棒支撑的，大多是纯冰里含点果汁，吮吸后便会失去甜味和口味，剩下白冰，较为廉价;雪糕：大多是碗装的，里面是黏体，需要汤勺取出，才你食用。",
+    readingNum:92,
+    collectionNum:{
+      array:[108]
+    },
+    commentNum:7,*/
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var iceCreamData={
+        object: {
+          date: "October 29 2019"
+        },
+        title: "小时候的冰棍和雪糕",
+        postImg: "/images/post/post-4.jpg",
+        avatar: "/images/avatar/avatar-5.png",
+        content: "冰棍和雪糕绝对不是同一个东西,冰棒： 有木棒支撑的，大多是纯冰里含点果汁，吮吸后便会失去甜味和口味，剩下白冰，较为廉价;雪糕：大多是碗装的，里面是黏体，需要汤勺取出，才你食用。",
+        readingNum: 92,
+        collectionNum: {
+          array: [108]
+        },
+        commentNum: 7,
+      }
 
+      this.setData({
+        postData:iceCreamData
+      })
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
+   * Note:在这里settitle,会发现界面有闪烁——刷新，因此不建议这样操作，这里仅仅是demo测试一下
    */
   onReady: function () {
-
+    /*this.setData({
+        title:"一根雪糕的经济原理"
+      })*/
   },
 
   /**
