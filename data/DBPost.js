b59259dc7cf70b9c6ce0a3c0fec73a37a49a1e3f
rick_tan <rick_tan@qq.com>
2019-10-31 23:01:25 +0800

    添加收藏和点赞功能

--- data/DBPost.js
+++ data/DBPost.js
@@ -5,6 +5,7 @@ class DBPost{
 
   //获取指定id号的文章数据
   getPostItemById(id) {
+    this.cacheId = id;
     var postsData = this.getAllPostData();
     var len = postsData.length;
     for (var i = 0; i < len; i++) {
@@ -22,14 +23,61 @@ class DBPost{
     var res = wx.getStorageSync(this.storageKeyName);
     if (!res) {
       res = require('../data/defaultdata.js').postList;
-      this.initPostList(res);
+      this.execSetStorageSync(res);
     }
     return res;
   }
 
   //本地缓存，保存/更新
-  initPostList(data) {
+  execSetStorageSync(data) {
     wx.setStorageSync(this.storageKeyName, data)
+  }
+  
+  //收藏文章
+  collect(id){
+    return this.updatePostData(id,'collect');
+  }
+
+  //点赞或取消点赞
+  up(id){
+    return this.updatePostData(id, 'up');
+  }
+
+  //更新本地的点赞、评论信息、收藏、阅读量
+  updatePostData(id, category){
+    var itemData = this.getPostItemById(id), postData = itemData.data, allPostData = this.getAllPostData();
+
+    switch (category){
+      case 'collect':
+        //处理收藏
+        if(!postData.collectionStatus){
+          //如果当前是未收藏
+          postData.collectionNum++;
+          postData.collectionStatus = true;
+        } else {
+          //如果当前是收藏
+          postData.collectionNum--; 
+          postData.collectionStatus = false;
+        }
+        break;
+      case 'up':
+        if(!postData.upStatus){
+          postData.upNum++;
+          postData.upStatus = true;
+        } else {
+          postData.upNum--;
+          postData.upStatus = false;
+        }
+        break;
+      default:
+        break;
+    }
+
+    //更新缓存数据
+    allPostData[itemData.index] = postData;
+    this.execSetStorageSync(allPostData);
+
+    return postData;
   }
 };
 
--- pages/post/detail/detail.js
+++ pages/post/detail/detail.js
@@ -15,22 +15,58 @@ Page({
    */
   onLoad: function (options) {
     // 注意这里的id变量是post.js启动detail页面的时候指定的
-    var postId = options.id;
+    this.postId = options.id;
     this.dbPost = new DBPost();
-    this.postData = this.dbPost.getPostItemById(postId).data;
+    this.postData = this.dbPost.getPostItemById(this.postId).data;
+
     this.setData({
       post: this.postData
     })
   },
 
+  onCollectionTap: function (event) {
+    //dbPost对象已经在onLoad函数里被保存到了this变量中，无须再次实例化
+    var newData = this.dbPost.collect(this.postId);
+
+    //重新绑定数据。注意，不要将整个newData全部作为setData的参数，应当有选择的更新部分数据
+    this.setData({
+      'post.collectionStatus':newData.collectionStatus,
+      'post.collectionNum':newData.collectionNum
+    })
+
+    wx.showToast({
+      title: newData.collectionStatus ? "收藏成功" : "取消成功",
+      duration: 1000,
+      icon: "success",
+      mask: true
+    })
+  },
+
+  onUpTap: function (event) {
+    //dbPost对象已经在onLoad函数里被保存到了this变量中，无须再次实例化
+    var newData = this.dbPost.up(this.postId);
+
+    //重新绑定数据。注意，不要将整个newData全部作为setData的参数，应当有选择的更新部分数据
+    this.setData({
+      'post.upStatus': newData.upStatus,
+      'post.upNum': newData.upNum
+    })
+
+    wx.showToast({
+      title: newData.upStatus ? "点赞成功" : "取消点赞",
+      duration: 1000,
+      icon: "success",
+      mask: true
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
     console.log("post Detail onReady!");
     wx.setNavigationBarTitle({
-      title: this.postData.title,
-    });
+      title: this.postData.title
+    })
   },
 
   /**
